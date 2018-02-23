c3284b7f9dc2cb49778298fcbc298741c184720b
donnysong <672010902@qq.com>
2018-02-23 10:58:19 +0800

    change package

--- src/main/java/com/hadoop/demo/filetest/FileCopyWithProcess.java
+++ src/main/java/com/hadoop/demo/filetest/FileCopyWithProcess.java
@@ -7,6 +7,7 @@ import org.apache.hadoop.io.IOUtils;
 import org.apache.hadoop.util.Progressable;
 import org.apache.log4j.Logger;
 
+import java.io.*;
 import java.net.URI;
 //bin/hadoop --loglevel DEBUG com.hadoopdemo.filetest.FileCopyWithProcess test.txt hdfs://localhost:9003/user/root/input/test.txt
 //bin/hadoop fs -cat input/test.txt
