57b392b5d1f43094f0d224f98a7da979df22f1cb
donnysong <672010902@qq.com>
2018-02-23 14:38:52 +0800

    test

--- src/main/java/com/hadoopdemo/conf/Configuration1.java
+++ src/main/java/com/hadoopdemo/conf/Configuration1.java
@@ -7,12 +7,12 @@ import java.io.IOException;
 
 public class Configuration1 {
     private static Logger logger = Logger.getLogger(Configuration1.class);
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Configuration conf = new Configuration();
         conf.addResource("configuration-1.xml");
 //        conf.writeXml(System.out);
-        //assertThat(conf.get("color"), is("yellow"));
-        System.setProperty("size", "16");
+//        assertThat(conf.get("color"), is("yellow"));
+//        System.setProperty("size", "16");
         logger.error(conf.get("color"));
         logger.error(conf.get("size"));
     }
